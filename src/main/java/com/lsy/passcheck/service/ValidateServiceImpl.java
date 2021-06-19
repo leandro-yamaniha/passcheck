143cd6c9b4ca530ed0294bcbaa75188f30b48b6d
leandro.yamaniha <leandro.yamaniha@luizalabs.com>
2021-06-19 15:51:28 -0300

    adjust regex

--- src/main/java/com/lsy/passcheck/service/ValidateServiceImpl.java
+++ src/main/java/com/lsy/passcheck/service/ValidateServiceImpl.java
@@ -7,10 +7,12 @@ import java.util.regex.Pattern;
 @Service
 public class ValidateServiceImpl implements ValidateService {
 
+    public static final String REGEX = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()+\\-])(?:([0-9a-zA-Z!@#$%^&*()+\\-])(?!.*\\1)){9,}$";
+
     @Override
     public boolean isValid(final String password) {
         return Pattern.matches(
-                "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()+\\-])(?:([0-9a-zA-Z!@#$%^&*()+\\-])(?!\\1)){9,}$",
+                REGEX,
                 password);
     }
 
