be32bfb9cee32c89acefda96687a376dd9ebd7ef
leandro-yamaniha <leandrosy@gmail.com>
2021-06-21 22:36:21 -0300

    remove constructor and set in ErrorDto

--- src/main/java/com/lsy/passcheck/dto/ErrorDto.java
+++ src/main/java/com/lsy/passcheck/dto/ErrorDto.java
@@ -7,20 +7,12 @@ public class ErrorDto {
 
     private Map<String, String> errors = new HashMap<>();
 
-    public ErrorDto() {
-
-    }
-
     public ErrorDto(final Map<String, String> errors) {
         this.errors = errors;
     }
 
     public ErrorDto(final String message) {
         this.errors.put("message", message);
-    }
-
-    public void setErrorMessage(final Map<String, String> errors) {
-        this.errors = errors;
     }
 
     public Map<String, String> getErrors() {
